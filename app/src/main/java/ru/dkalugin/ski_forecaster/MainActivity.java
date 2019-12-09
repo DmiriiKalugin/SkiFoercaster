8621a5015e8605198cf1e4ea4b414cf686d9156d
Dmitry_Kalugin <kalugindmitrii089@gmail.com>
2019-12-09 23:37:07 +0500

    To create first actitvity

--- app/src/main/java/ru/dkalugin/ski_forecaster/MainActivity.java
+++ app/src/main/java/ru/dkalugin/ski_forecaster/MainActivity.java
@@ -2,7 +2,11 @@ package ru.dkalugin.ski_forecaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button next = (Button) findViewById(R.id.next_activity);
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent_next_activity = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent_next_activity);
+            }
+        });
+
+
     }
 }
