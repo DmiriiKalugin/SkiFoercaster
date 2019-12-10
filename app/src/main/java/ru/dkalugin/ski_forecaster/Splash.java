72da36e27f830db79224d688ca2defad21e1b2a6
Dmitry_Kalugin <kalugindmitrii089@gmail.com>
2019-12-10 17:31:30 +0500

    To create first activity

--- app/src/main/java/ru/dkalugin/ski_forecaster/Splash.java
+++ app/src/main/java/ru/dkalugin/ski_forecaster/Splash.java
@@ -3,22 +3,21 @@ package ru.dkalugin.ski_forecaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
         Button next = (Button) findViewById(R.id.next_activity);
         next.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent_next_activity = new Intent(MainActivity.this, Main2Activity.class);
+                Intent intent_next_activity = new Intent(Splash.this, First.class);
                 startActivity(intent_next_activity);
             }
         });
