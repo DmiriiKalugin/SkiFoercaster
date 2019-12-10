ee8c93d623304935a3b07626afbfce60811a3797
Dmitry_Kalugin <kalugindmitrii089@gmail.com>
2019-12-10 17:56:09 +0500

    To create splash activity

--- app/src/main/java/ru/dkalugin/ski_forecaster/Splash.java
+++ app/src/main/java/ru/dkalugin/ski_forecaster/Splash.java
@@ -7,21 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.concurrent.TimeUnit;
+
 public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Button next = (Button) findViewById(R.id.next_activity);
-        next.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent_next_activity = new Intent(Splash.this, First.class);
-                startActivity(intent_next_activity);
-            }
-        });
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-
+        Intent intent = new Intent(this, First.class);
+        startActivity(intent);
+        finish();
     }
 }
