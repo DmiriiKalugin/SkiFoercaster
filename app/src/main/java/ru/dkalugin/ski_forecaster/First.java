15262881cf4d68f4e15f0a6ba00553263975e41a
Dmitry_Kalugin <kalugindmitrii089@gmail.com>
2019-12-16 22:10:54 +0500

    Upgrade

--- app/src/main/java/ru/dkalugin/ski_forecaster/First.java
+++ app/src/main/java/ru/dkalugin/ski_forecaster/First.java
@@ -2,13 +2,21 @@ package ru.dkalugin.ski_forecaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class First extends AppCompatActivity {
+public class First extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+
     }
+
+
+
 }
