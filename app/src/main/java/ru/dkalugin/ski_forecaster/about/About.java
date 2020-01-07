4f8440532962496adcffcf7a17369c5107104829
Dmitry_Kalugin <kalugindmitrii089@gmail.com>
2020-01-07 20:42:17 +0500

    Creating an about page

---
+++ app/src/main/java/ru/dkalugin/ski_forecaster/about/About.java
@@ -0,0 +1,16 @@
+package ru.dkalugin.ski_forecaster.about;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.dkalugin.ski_forecaster.R;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/ru/dkalugin/ski_forecaster/ski_forecaster/SkiForecaster.java
+++ app/src/main/java/ru/dkalugin/ski_forecaster/ski_forecaster/SkiForecaster.java
@@ -34,6 +34,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import ru.dkalugin.ski_forecaster.R;
+import ru.dkalugin.ski_forecaster.about.About;
 import ru.dkalugin.ski_forecaster.condition.Arcificial;
 import ru.dkalugin.ski_forecaster.condition.Crude;
 import ru.dkalugin.ski_forecaster.condition.Fresh;
@@ -69,6 +70,10 @@ public class   SkiForecaster extends AppCompatActivity  implements View.OnClickL
         btn_old.setOnClickListener(this);
         btn_crude.setOnClickListener(this);
         btn_artificial.setOnClickListener(this);
+
+        TextView about_list = (TextView) findViewById(R.id.about_list);
+
+        about_list.setOnClickListener(this);
 
         Date date = new Date();
 
@@ -152,57 +157,75 @@ public class   SkiForecaster extends AppCompatActivity  implements View.OnClickL
 
         EditText air_temperature = (EditText) findViewById(R.id.air_temperature);
 
-        if (air_temperature.getText().toString().equals(""))
-        {
-            openSiteDialog();
-        }
-        else
-        {
-            String temperature = air_temperature.getText().toString();
 
-            final ProgressDialog progressDialog = new ProgressDialog(SkiForecaster.this, R.style.MyTheme);
-            progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Small);
-            progressDialog.show();
+        String temperature = air_temperature.getText().toString();
+
+        final ProgressDialog progressDialog = new ProgressDialog(SkiForecaster.this, R.style.MyTheme);
+        progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Small);
+        progressDialog.show();
 //        After 2 Seconds i dismiss progress Dialog
 
-            new Thread(){
-                @Override
-                public void run() {
-                    super.run();
-                    try {
-                        Thread.sleep(2000);
-                        if (progressDialog.isShowing())
-                            progressDialog.dismiss();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    Thread.sleep(2000);
+                    if (progressDialog.isShowing())
+                        progressDialog.dismiss();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            }.start();
-
-
-            switch (v.getId()){
-                case R.id.btn_fresh:
-                    Intent fresh = new Intent(this, Fresh.class);
-                    fresh.putExtra("temperature", temperature);
-                    startActivity(fresh);
-                    break;
-                case R.id.btn_old:
-                    Intent old = new Intent(this, Old.class);
-                    old.putExtra("temperature", temperature);
-                    startActivity(old);
-                    break;
-                case R.id.btn_crude:
-                    Intent crude = new Intent(this, Crude.class);
-                    crude.putExtra("temperature", temperature);
-                    startActivity(crude);
-                    break;
-                case R.id.btn_artificial:
-                    Intent artificial = new Intent(this, Arcificial.class);
-                    artificial.putExtra("temperature", temperature);
-                    startActivity(artificial);
-                    break;
             }
+        }.start();
+
+        switch (v.getId()){
+            case R.id.btn_fresh:
+                Intent fresh = new Intent(this, Fresh.class);
+                fresh.putExtra("temperature", temperature);
+                if(air_temperature.getText().toString().equals("")){
+                    openSiteDialog();
+                }
+                else {
+                    startActivity(fresh);
+                }
+
+                break;
+            case R.id.btn_old:
+                Intent old = new Intent(this, Old.class);
+                old.putExtra("temperature", temperature);
+                if(air_temperature.getText().toString().equals("")){
+                    openSiteDialog();
+                }
+                else {
+                    startActivity(old);
+                }
+                break;
+            case R.id.btn_crude:
+                Intent crude = new Intent(this, Crude.class);
+                crude.putExtra("temperature", temperature);
+                if(air_temperature.getText().toString().equals("")){
+                    openSiteDialog();
+                }
+                else {
+                    startActivity(crude);
+                }
+                break;
+            case R.id.btn_artificial:
+                Intent artificial = new Intent(this, Arcificial.class);
+                artificial.putExtra("temperature", temperature);
+                if(air_temperature.getText().toString().equals("")){
+                    openSiteDialog();
+                }
+                else {
+                    startActivity(artificial);
+                }
+                break;
+
+            case R.id.about_list:
+                Intent about = new Intent(this, About.class);
+                startActivity(about);
+                break;
         }
     }
-
 }
